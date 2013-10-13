62e32684ff3580e792513168e7409de1d9394e29
Gina Trapani <ginatrapani@gmail.com>
2013-10-12 22:37:11 -0700

    Enable Smarty caching to file

--- webapp/libs/controller/class.PledgeController.php
+++ webapp/libs/controller/class.PledgeController.php
@@ -2,9 +2,11 @@
 class PledgeController extends Controller {
     public function control() {
         $this->setViewTemplate('index.tpl');
-        $subscriber_count_dao = new SubscriberCountMySQLDAO();
-        $subscriber_counts = $subscriber_count_dao->getAll();
-        $this->addToView('subscriber_counts', $subscriber_counts);
+        if ($this->shouldRefreshCache() ) {
+            $subscriber_count_dao = new SubscriberCountMySQLDAO();
+            $subscriber_counts = $subscriber_count_dao->getAll();
+            $this->addToView('subscriber_counts', $subscriber_counts);
+        }
         return $this->generateView();
     }
 }
