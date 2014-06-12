6070f1d87986647cd655d5ce3871043acd7baceb
Gina Trapani <ginatrapani@gmail.com>
2014-06-12 11:58:45 -0400

    Add fourth payment reminder email (needs copy)

--- tests/TestOfMailer.php
+++ tests/TestOfMailer.php
@@ -2,7 +2,7 @@
 require_once dirname(__FILE__) . '/init.tests.php';
 require_once ISOSCELES_PATH.'extlibs/simpletest/autorun.php';
 
-class TestOfMailer extends UpstartUnitTestCase {
+class TestOfMailer extends UpstartBasicUnitTestCase {
 
     public function setUp() {
         parent::setUp();
@@ -86,6 +86,16 @@ class TestOfMailer extends UpstartUnitTestCase {
         $email_view_mgr->caching=false;
         $email_view_mgr->assign('amazon_error_message', "Card has Expired" );
         $body_html = $email_view_mgr->fetch('_email.payment-charge-failure.tpl');
+        $message = Mailer::getSystemMessageHTML($body_html, $headline);
+        $this->debug($message);
+    }
+
+    public function testGetSystemMessageHTMLPaymentReminder4() {
+        $headline = "We don’t want to say goodbye so soon!";
+        $email_view_mgr = new ViewManager();
+        $email_view_mgr->caching=false;
+        $email_view_mgr->assign('thinkup_username', 'username' );
+        $body_html = $email_view_mgr->fetch('_email.payment-reminder-fourth.tpl');
         $message = Mailer::getSystemMessageHTML($body_html, $headline);
         $this->debug($message);
     }
