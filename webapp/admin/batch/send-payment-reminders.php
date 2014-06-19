079abff2d4156eb0b261d0b04419618643f063c4
Gina Trapani <ginatrapani@gmail.com>
2014-06-19 15:45:44 -0400

    Add free trial email reminders to batch job

--- webapp/admin/batch/send-payment-reminders.php
+++ webapp/admin/batch/send-payment-reminders.php
@@ -5,3 +5,6 @@ require_once 'init.php';
 
 $controller = new RemindAbandonsAboutPaymentController();
 $controller->control();
+
+$controller = new RemindTrialersAboutPaymentController();
+$controller->control();
