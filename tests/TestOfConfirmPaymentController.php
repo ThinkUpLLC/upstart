5d2c63590ebf855fbefbe4a81b4eb85a2faa5ec8
Gina Trapani <ginatrapani@gmail.com>
2014-06-20 11:43:57 -0400

    Beef up TestOfConfirmPaymentController

--- tests/TestOfConfirmPaymentController.php
+++ tests/TestOfConfirmPaymentController.php
@@ -1,6 +1,7 @@
 <?php
 require_once dirname(__FILE__) . '/init.tests.php';
 require_once ISOSCELES_PATH.'extlibs/simpletest/autorun.php';
+require_once dirname(__FILE__) . '/classes/mock.AmazonFPSAPIAccessor.php';
 
 class TestOfConfirmPaymentController extends UpstartUnitTestCase {
 
@@ -15,5 +16,39 @@ class TestOfConfirmPaymentController extends UpstartUnitTestCase {
     public function testConstructor() {
         $controller = new ConfirmPaymentController(true);
         $this->assertIsA($controller, 'ConfirmPaymentController');
+    }
+
+    public function testNoReturnFromAmazon() {
+        $controller = new ConfirmPaymentController(true);
+        $results = $controller->go();
+        $this->debug($results);
+        $this->assertPattern('/Subscriber ID  does not exist./', $results);
+    }
+
+    public function testReturnFromAmazonValidSignature() {
+        $builders = $this->buildData();
+        SessionCache::put('new_subscriber_id', 6);
+
+        $_GET['callerReference'] = 'abcde';
+        $_GET['tokenID'] = 'token1';
+        $_GET['level'] = 'member';
+        $_GET['status'] = "SC";
+        $_GET['certificateUrl'] = "certificate1";
+        $_GET['signatureMethod'] = "SHA";
+        $_GET['signature'] = 'signature1';
+
+        $controller = new ConfirmPaymentController(true);
+        $results = $controller->go();
+        $this->debug($results);
+        $this->assertNoPattern('/Subscriber ID  does not exist./', $results);
+        $this->assertPattern('/delighted to have you as part of the ThinkUp community/', $results);
+    }
+
+    protected function buildData() {
+        $builders = array();
+        $builders[] = FixtureBuilder::build('subscribers', array('id'=>6, 'email'=>'me@example.com',
+            'is_activated'=>1, 'is_admin'=>1, 'thinkup_username'=>'iamtaken', 'membership_level'=>'Member'));
+
+        return $builders;
     }
 }
