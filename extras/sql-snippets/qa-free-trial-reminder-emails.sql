3db07c220a77a01ae1a647614636eaf9aff92eb2
Gina Trapani <ginatrapani@gmail.com>
2014-07-08 13:12:57 -0400

    Dev: SQL snippet to QA free trial email

---
+++ extras/sql-snippets/qa-free-trial-reminder-emails.sql
@@ -0,0 +1,23 @@
+#
+# Fake-test free trial reminder emails on staging by fast-forwarding signup/reminder times and running email job.
+#
+
+#
+# First reminder on day 1
+#
+UPDATE subscribers SET creation_time = DATE_SUB(creation_time, INTERVAL 2 DAY);
+
+#
+# Second reminder on day 7
+#
+UPDATE subscribers SET payment_reminder_last_sent = DATE_SUB(payment_reminder_last_sent, INTERVAL 145 HOUR);
+
+#
+# Third reminder on day 13
+#
+UPDATE subscribers SET payment_reminder_last_sent = DATE_SUB(payment_reminder_last_sent, INTERVAL 145 HOUR);
+
+#
+# Fourth and final reminder on day 14
+#
+UPDATE subscribers SET payment_reminder_last_sent = DATE_SUB(payment_reminder_last_sent, INTERVAL 25 HOUR);
