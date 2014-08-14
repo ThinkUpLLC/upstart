ac17e9d7bee80e466eb43395328fc9286d1f6711
Gina Trapani <ginatrapani@gmail.com>
2014-08-14 10:41:30 -0400

    APIIPNController: Fix "Undefined index: signature" error

--- webapp/libs/controller/class.APIIPNController.php
+++ webapp/libs/controller/class.APIIPNController.php
@@ -6,19 +6,23 @@ class APIIPNController extends UpstartController {
 
         $ipn_endpoint = Config::getInstance()->getValue('amazon_ipn_endpoint');
 
-        try {
-            $debug = '';
-            //IPN is sent as a http POST request and hence we specify POST as the http method.
-            //Signature verification does not require your secret key
-            if ($amazon_ipn_utils->validateRequest($_POST, $ipn_endpoint, "POST")) {
-                $debug .= "Signature correct. ";
-            } else {
-                $debug .= "Signature not correct. ";
-            }
-            $debug .= Utils::varDumpToString($_POST);
+        if (isset($_POST['signature'])) {
+            try {
+                $debug = '';
+                //IPN is sent as a http POST request and hence we specify POST as the http method.
+                //Signature verification does not require your secret key
+                if ($amazon_ipn_utils->validateRequest($_POST, $ipn_endpoint, "POST")) {
+                    $debug .= "Signature correct. ";
+                } else {
+                    $debug .= "Signature not correct. ";
+                }
+                $debug .= Utils::varDumpToString($_POST);
 
-        } catch (Exception $e ) {
-            $debug = $e->getMessage();
+            } catch (Exception $e ) {
+                $debug = $e->getMessage();
+            }
+        } else {
+            $debug = "Signature is not set.";
         }
         $this->logError($debug, __FILE__,__LINE__,__METHOD__);
         //echo $debug;
