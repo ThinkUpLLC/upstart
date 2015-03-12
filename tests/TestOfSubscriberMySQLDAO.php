b505b723d938b321684b40857aa629850e90c109
Gina Trapani <ginatrapani@gmail.com>
2015-03-12 12:10:38 -0400

    Reserve system subdomain images.thinkup.com

--- tests/TestOfSubscriberMySQLDAO.php
+++ tests/TestOfSubscriberMySQLDAO.php
@@ -419,6 +419,7 @@ class TestOfSubscriberMySQLDAO extends UpstartUnitTestCase {
         $this->assertTrue($dao->isUsernameTaken('demo'));
         $this->assertTrue($dao->isUsernameTaken('book'));
         $this->assertTrue($dao->isUsernameTaken('shares'));
+        $this->assertTrue($dao->isUsernameTaken('images'));
         $this->assertFalse($dao->isUsernameTaken('cordeliachase'));
 
         $builders = array();
--- webapp/libs/dao/class.SubscriberMySQLDAO.php
+++ webapp/libs/dao/class.SubscriberMySQLDAO.php
@@ -813,7 +813,7 @@ class SubscriberMySQLDAO extends PDODAO {
      * @return bool  Whether or not it is in use
      */
     public function isUsernameTaken($thinkup_username) {
-        $reserved_names = array('www', 'stage', 'demo', 'book', 'shares');
+        $reserved_names = array('www', 'stage', 'demo', 'book', 'shares', 'images');
         if (in_array($thinkup_username, $reserved_names) ) {
             return true;
         }
