56f7d2077c7d8b1f2de7f365da558981d7507305
Gina Trapani <ginatrapani@gmail.com>
2015-03-12 11:34:16 -0400

    Fix bouce-to-login redirect bug

--- webapp/libs/controller/class.UpstartAuthController.php
+++ webapp/libs/controller/class.UpstartAuthController.php
@@ -1,6 +1,13 @@
 <?php
 
 abstract class UpstartAuthController extends AuthController {
+
+    public function __construct($session_started=false) {
+        parent::__construct($session_started);
+        if (isset($_SERVER['REQUEST_URI']) && strpos($_SERVER['REQUEST_URI'], 'logout.php') === false ) {
+            $this->url_mapping = 'http://'.UpstartHelper::getApplicationHostName().$_SERVER['REQUEST_URI'];
+        }
+    }
     /**
      * Bounce user to log in.
      * A child class can override this method to define different bounce behavior.
