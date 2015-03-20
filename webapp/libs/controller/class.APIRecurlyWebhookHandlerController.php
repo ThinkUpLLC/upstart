1e6690f3f937e6d6cfe87664ba864e42012333d2
Gina Trapani <ginatrapani@gmail.com>
2015-03-20 12:03:53 -0400

    Recurly web hooks: Improve parsing

--- webapp/libs/controller/class.APIRecurlyWebhookHandlerController.php
+++ webapp/libs/controller/class.APIRecurlyWebhookHandlerController.php
@@ -3,8 +3,29 @@
 class APIRecurlyWebhookHandlerController extends Controller {
 
     public function control() {
-        //@TODO Smartly handle post vars, just capturing and logging for now for testing purposes
-        $debug = "Received Recurly Webhook";
+        //https://docs.recurly.com/client-libraries/php#handle_webhooks
+
+        //Recurly will POST an XML payload to your URL that you designate
+        //in your webhooks configuration
+
+        //Get the XML Payload
+        $post_xml = file_get_contents ("php://input");
+        $notification = new Recurly_PushNotification($post_xml);
+        //@TODO Smartly handle various notification types, just capturing and logging for now for testing purposes
+        //each webhook is defined by a type
+        // switch ($notification->type) {
+        //   case "successful_payment_notification":
+        //     /* process notification here */
+        //     break;
+        //   case "failed_payment_notification":
+        //     /* process notification here */
+        //     break;
+        //   /* add more notifications to process */
+        // }
+
+        $debug = "Received Recurly Webhook
+";
+        $debug .= Utils::varDumpToString($notification);
         Logger::logError($debug, __FILE__,__LINE__,__METHOD__);
     }
 }
