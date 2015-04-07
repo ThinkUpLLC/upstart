bd6c413744a06ef92c309018f82ddda455633c9b
Gina Trapani <ginatrapani@gmail.com>
2015-04-07 16:34:32 -0400

    Image proxycacher: Add comparator function

--- webapp/img.php
+++ webapp/img.php
@@ -1,8 +1,10 @@
 <?php
 require_once 'init.php';
 /**
- * Image proxy and cache
+ * Image proxy and cache, and comparator
  *
+ * Proxy-cache
+ * -----------
  * Accepts 3 $_GET parameters:
  * t (type) = either 'avatar' or 'favicon'
  * url = valid URL
@@ -23,6 +25,18 @@ require_once 'init.php';
  *
  * v 2.0
  * Add support for favicons
+ *
+ * Comparator
+ * ----------
+ * Accepts 3 $_GET parameters
+ * * image1
+ * * image2
+ * * s (signature) = a token proving the loader is legit
+ *
+ * v 1.0
+ * If request has a valid signature and URLs for two images:
+ * If both images exist on file system and reasonably match
+ *  return JSON result show_diff and reason
  */
 
 class ImageProxyCacheController extends Controller {
@@ -38,7 +52,7 @@ class ImageProxyCacheController extends Controller {
     var $local_filename;
 
     public function control() {
-        if ($this->isValidRequest()) {
+        if ($this->isValidImageRequest()) {
             $this->url = $_GET['url'];
             $parsed_url = parse_url($this->url);
 
@@ -63,6 +77,35 @@ class ImageProxyCacheController extends Controller {
             } else {
                 $this->redirect($this->url);
             }
+        } elseif ($this->isValidComparisonRequest()) {
+            $url_a = $_GET['image1'];
+            $parsed_url_a = parse_url($url_a);
+            $url_path_a = $parsed_url_a['path'];
+            $url_extension_a = pathinfo($url_path_a, PATHINFO_EXTENSION);
+            $file_a = $this->getLocalFilename($url_a, $url_extension_a);
+
+            $url_b = $_GET['image2'];
+            $parsed_url_b = parse_url($url_b);
+            $url_path_b = $parsed_url_b['path'];
+            $url_extension_b = pathinfo($url_path_b, PATHINFO_EXTENSION);
+            $file_b = $this->getLocalFilename($url_b, $url_extension_b);
+
+            $reason = '';
+            $show_diff = false;
+            if (file_exists($file_a) && file_exists($file_b)) {
+                if ($this->doFilesMatch($file_a, $file_b)) {
+                    $show_diff = false;
+                    $reason = 'Files match';
+                } else {
+                    $show_diff = true;
+                    $reason = 'Files do no match';
+                }
+            } else {
+                $show_diff = false;
+                $reason = 'One or both files do not exist';
+            }
+            $this->setJsonData(array('show_diff'=>$show_diff, 'reason'=>$reason));
+            return $this->generateView();
         } else {
             $this->redirect($this->url);
         }
@@ -71,16 +114,36 @@ class ImageProxyCacheController extends Controller {
      * Check if the request for an image is valid - has all required parameters and a valid signature.
      * @return bool
      */
-    public function isValidRequest() {
+    private function isValidImageRequest() {
+        return (
+            $this->isSignatureValid()
+            && isset($_GET['url'])
+            //image type
+            && (isset($_GET['t']) && $_GET['t'] == 'avatar')
+        );
+    }
+    /**
+     * Check if the request for an image is valid - has all required parameters and a valid signature.
+     * @return bool
+     */
+    private function isValidComparisonRequest() {
+        return (
+            $this->isSignatureValid()
+            && isset($_GET['image1'])
+            && isset($_GET['image2'])
+        );
+    }
+    /**
+     * Check if the signature is valid.
+     * @return bool
+     */
+    private function isSignatureValid() {
         $cfg = Config::getInstance();
         $passphrase = $cfg->getValue('image_proxy_passphrase');
 
         return (
-            isset($_GET['url'])
-            //image type
-            && (isset($_GET['t']) && $_GET['t'] == 'avatar')
             //request signature is a simple MD5 hash of a passphrase
-            && (isset($_GET['s']) && $_GET['s'] == md5($passphrase))
+            isset($_GET['s']) && $_GET['s'] == md5($passphrase)
         );
     }
     /**
@@ -89,7 +152,7 @@ class ImageProxyCacheController extends Controller {
      * @param  str $extension File extension of image
      * @return str Full local file path
      */
-    public function getLocalFilename($url, $extension) {
+    private function getLocalFilename($url, $extension) {
         $data_path = FileDataManager::getDataPath();
         $local_file = $data_path.'image-cache/avatars/'.(md5($url).'.'.$extension);
         return $local_file;
@@ -103,7 +166,7 @@ class ImageProxyCacheController extends Controller {
      * @param  str $extension
      * @return void
      */
-    public function cacheAndDisplayImage($url, $local_filename, $blank, $extension){
+    private function cacheAndDisplayImage($url, $local_filename, $blank, $extension){
         $ch = curl_init ($url);
         curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -137,6 +200,32 @@ class ImageProxyCacheController extends Controller {
         header('Content-Type:'.$type);
         header('Content-Length: ' . filesize($file));
         readfile($file);
+    }
+    /**
+     * Tell whether or not 2 files are the same.
+     * This is a rudimentary, hit-or-miss file comparison.
+     * @param  str $file_a
+     * @param  str $file_b
+     * @return bool
+     */
+    private function doFilesMatch($file_a, $file_b) {
+        if (filesize($file_a) == filesize($file_b)) {
+            $fp_a = fopen($file_a, 'rb');
+            $fp_b = fopen($file_b, 'rb');
+
+            while (($b = fread($fp_a, 4096)) !== false) {
+                $b_b = fread($fp_b, 4096);
+                if ($b !== $b_b) {
+                    fclose($fp_a);
+                    fclose($fp_b);
+                    return false;
+                }
+            }
+            fclose($fp_a);
+            fclose($fp_b);
+            return true;
+        }
+        return false;
     }
 }
 
