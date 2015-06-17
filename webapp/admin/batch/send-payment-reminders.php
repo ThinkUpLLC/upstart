c0999f289efed66a4501c8f0c5cc9db267d07d34
Gina Trapani <ginatrapani@gmail.com>
2015-06-17 11:22:48 -0400

    Stop reminding FPS annuals that they will get auto-renewed

--- webapp/admin/batch/send-payment-reminders.php
+++ webapp/admin/batch/send-payment-reminders.php
@@ -6,6 +6,6 @@ require_once 'init.php';
 $controller = new RemindTrialersAboutPaymentController();
 $controller->control();
 
-//@TODO Disable this once Amazon FPS stops working (June 1)
-$controller = new RemindAnnualsAboutReupController();
-$controller->control();
+//Temporarily disabled until it's converted to FPS transition marketing messages
+// $controller = new RemindAnnualsAboutReupController();
+// $controller->control();
