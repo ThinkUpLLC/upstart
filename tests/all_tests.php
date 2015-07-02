56dc2ee0ae613a80f7b7b2cd5680bfdccbee7821
Gina Trapani <ginatrapani@gmail.com>
2015-07-02 11:03:58 -0400

    Deprecate unused Amazon Payments FPS code

--- tests/all_tests.php
+++ tests/all_tests.php
@@ -37,7 +37,6 @@ $all_tests->add(new TestOfRegisterController());
 $all_tests->add(new TestOfRemindTrialersAboutPaymentController());
 $all_tests->add(new TestOfRemindAnnualsAboutReupController());
 $all_tests->add(new TestOfMailer());
-$all_tests->add(new TestOfUpdatePendingPaymentStatusController());
 $all_tests->add(new TestOfPayNowController());
 $all_tests->add(new TestOfAPIIPNController());
 $all_tests->add(new TestOfClaimCodeMySQLDAO());
--- webapp/admin/batch/charge-annuals.php
+++
@@ -1,7 +0,0 @@
-<?php
-chdir('..');
-chdir('..');
-require_once 'init.php';
-
-$controller = new ChargeAnnualSubscribersController();
-$controller->control();
